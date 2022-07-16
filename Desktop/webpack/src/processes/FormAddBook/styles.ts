297d8ecec68d9608f0a267bb77f02fbdf6722066
Ilya Kislenko <62322166+cashcats69@users.noreply.github.com>
2022-07-17 00:32:00 +0700

    feat:Initial project version

---
+++ Desktop/webpack/src/processes/FormAddBook/styles.ts
@@ -0,0 +1,11 @@
+import { styled } from "@linaria/react";
+
+export const Form = styled.form`
+display:flex;
+flex-direction:column;
+justify-content:center;
+width:400px;
+background:#FFFFFF;
+border:1px solid #A8A8A8;
+border-radius:3px;
+`;
